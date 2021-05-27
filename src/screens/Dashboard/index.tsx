1b9595c6bf9b704bfb2bc7afa1f62f420410a8af
Jin Bok de Oliveira <jinoliveira74@gmail.com>
2021-05-27 10:46:14 -0300

    chore: cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
 
-import { Container, Title } from './styles'
+import {
+   Container,
+   Header,
+   UserWrapper,
+   UserInfo,
+   Photo,
+   User,
+   UserGreeting,
+   UserName,
+   Icon,
+   HighlightCards
+} from './styles'
 
 export function Dashboard() {
    return (
       <Container>
-         <Title>Dashboard</Title>
+         <Header>
+            <UserWrapper>
+               <UserInfo>
+                  <Photo source={{ uri: 'https://instagram.fcgh16-1.fna.fbcdn.net/v/t51.2885-19/s150x150/16908619_1788825871438456_3209390917249138688_a.jpg?tp=1&_nc_ht=instagram.fcgh16-1.fna.fbcdn.net&_nc_ohc=D9aW_21Hi5oAX-OpQSD&edm=ABfd0MgBAAAA&ccb=7-4&oh=9f38930a5aaae34b23744157520418ee&oe=60B607AC&_nc_sid=7bff83' }}/>
+                  <User>
+                     <UserGreeting>Olá,</UserGreeting>
+                     <UserName>Jin</UserName>
+                  </User>
+               </UserInfo>
+
+               <Icon name="power"/>
+            </UserWrapper>
+         </Header>
+
+         <HighlightCards>
+            <HighlightCard />
+            <HighlightCard />
+            <HighlightCard />
+         </HighlightCards>
       </Container>
    )
 }
